95bbbfa8a85123cef2869b6531db63377ae2bd97
PB2 <gitstuff2@gmail.com>
2020-04-09 03:05:21 -0400

    Added rx_obj() and tx_obj() members

--- pySerialTransfer/pySerialTransfer.py
+++ pySerialTransfer/pySerialTransfer.py
@@ -1,6 +1,9 @@
 import os
+import json
+import struct
 import serial
 import serial.tools.list_ports
+from array import array
 from .CRC import CRC
 
 
@@ -8,24 +11,24 @@ class InvalidSerialPort(Exception):
     pass
 
 
-CONTINUE = 2
-NEW_DATA = 1
-NO_DATA = 0
-CRC_ERROR = -1
-PAYLOAD_ERROR = -2
+CONTINUE        = 2
+NEW_DATA        = 1
+NO_DATA         = 0
+CRC_ERROR       = -1
+PAYLOAD_ERROR   = -2
 STOP_BYTE_ERROR = -3
 
 START_BYTE = 0x7E
-STOP_BYTE = 0x81
+STOP_BYTE  = 0x81
 
 MAX_PACKET_SIZE = 0xFE
 
-find_start_byte = 0
+find_start_byte    = 0
 find_overhead_byte = 1
-find_payload_len = 2
-find_payload = 3
-find_crc = 4
-find_end_byte = 5
+find_payload_len   = 2
+find_payload       = 3
+find_crc           = 4
+find_end_byte      = 5
 
 
 def msb(val):
@@ -155,6 +158,118 @@ class SerialTransfer(object):
         '''
         if self.connection.is_open:
             self.connection.close()
+    
+    def tx_obj(self, val, start_pos=0):
+        '''
+        Description:
+        -----------
+        Insert an arbitrary variable's value into the TX buffer starting at the
+        specified index
+        
+        :param val:       n/a   - value to be inserted into TX buffer
+        :param start_pos: int   - index of TX buffer where the first byte
+                                  of the value is to be stored in
+    
+        :return: int - index of the last byte of the value in the TX buffer + 1,
+                       None if operation failed
+        '''
+        
+        if type(val) == str:
+            val = val.encode()
+            format_str = '%ds' % len(val)
+            
+        elif type(val) == dict:
+            val = json.dumps(val).encode()
+            format_str = '%ds' % len(val)
+            
+        elif type(val) == float:
+            format_str = 'f'
+            
+        elif type(val) == int:
+            format_str = 'i'
+            
+        elif type(val) == bool:
+            format_str = '?'
+            
+        elif type(val) == list:
+            for el in val:
+                start_pos = self.tx_obj(el, start_pos)
+            
+            return start_pos
+        
+        else:
+            return None
+      
+        val_bytes = struct.pack(format_str, val)
+      
+        for index in range(len(val_bytes)):
+            self.txBuff[index + start_pos] = val_bytes[index]
+        
+        return start_pos + len(val_bytes)
+
+    def rx_obj(self, obj_type, obj_byte_size, start_pos=0, list_format=None):
+        '''
+        Description:
+        ------------
+        Extract an arbitrary variable's value from the RX buffer starting at
+        the specified index. If object_type is list, it is assumed that the
+        list to be extracted has homogeneous element types where the common
+        element type can neither be list, dict, nor string longer than a
+        single char
+        
+        :param obj_type:      type - type of object to extract from the RX buffer
+        :param obj_byte_size: int  - number of bytes making up extracted object
+        :param start_pos:     int  - index of TX buffer where the first byte
+                                     of the value is to be stored in
+        :param list_format:   char - array.array format char to represent the
+                                     common list element type - 'c' for a char
+                                     list is supported
+    
+        :return unpacked_response: obj - object extracted from the RX buffer,
+                                         None if operation failed
+        '''
+        
+        buff = bytes(self.rxBuff[start_pos:(start_pos+obj_byte_size)])
+        
+        if (obj_type == str) or (obj_type == dict):
+            format_str = '%ds' % len(buff)
+            
+        elif obj_type == float:
+            format_str = 'f'
+            
+        elif obj_type == int:
+            format_str = 'i'
+            
+        elif obj_type == bool:
+            format_str = '?'
+            
+        elif obj_type == list:
+            if list_format == 'c':
+                arr = array('B', buff)
+                temp_list = arr.tolist()
+                
+                for index in range(len(temp_list)):
+                    temp_list[index] = chr(temp_list[index])
+                return temp_list
+            
+            elif list_format:
+                arr = array(list_format, buff)
+                return arr.tolist()
+            
+            else:
+                return None
+        else:
+            return None
+        
+        unpacked_response = struct.unpack(format_str, buff)[0]
+        
+        if (obj_type == str) or (obj_type == dict):
+            unpacked_response = unpacked_response.decode('utf-8')
+        
+        if obj_type == dict:
+            unpacked_response = json.loads(unpacked_response)
+        
+        return unpacked_response
 
     def calc_overhead(self, pay_len):
         '''

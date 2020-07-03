ac3c65d6971bc27ea1c76a0e8ca99bb07f9e3bd0
PB2 <gitstuff2@gmail.com>
2020-07-02 22:58:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name         = 'pySerialTransfer',
     packages     = ['pySerialTransfer'],
-    version      = '2.0.0',
+    version      = '2.0.1',
     description  = 'Python package used to transmit and receive low overhead byte packets - especially useful for PC<-->Arduino USB communication (compatible with https://github.com/PowerBroker2/SerialTransfer)',
     author       = 'Power_Broker',
     author_email = 'gitstuff2@gmail.com',

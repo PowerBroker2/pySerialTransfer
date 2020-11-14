1dce25372726bd0e3a8e56899f2ee7c2b5006efb
PB2 <gitstuff2@gmail.com>
2020-11-14 11:41:06 -0500

    Add long description

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name         = 'pySerialTransfer',
-    packages     = ['pySerialTransfer'],
-    version      = '2.0.4',
-    description  = 'Python package used to transmit and receive low overhead byte packets - especially useful for PC<-->Arduino USB communication (compatible with https://github.com/PowerBroker2/SerialTransfer)',
-    author       = 'Power_Broker',
-    author_email = 'gitstuff2@gmail.com',
-    url          = 'https://github.com/PowerBroker2/pySerialTransfer',
-    download_url = 'https://github.com/PowerBroker2/pySerialTransfer/archive/2.0.4.tar.gz',
-    keywords     = ['Arduino', 'serial', 'usb', 'protocol', 'communication'],
-    classifiers  = [],
-    install_requires=['pyserial']
+    name             = 'pySerialTransfer',
+    packages         = ['pySerialTransfer'],
+    version          = '2.0.4',
+    description      = 'Python package used to transmit and receive low overhead byte packets - especially useful for PC<-->Arduino USB communication (compatible with https://github.com/PowerBroker2/SerialTransfer)',
+    long_description = long_description,
+    long_description_content_type = "text/plain",
+    author           = 'Power_Broker',
+    author_email     = 'gitstuff2@gmail.com',
+    url              = 'https://github.com/PowerBroker2/pySerialTransfer',
+    download_url     = 'https://github.com/PowerBroker2/pySerialTransfer/archive/2.0.4.tar.gz',
+    keywords         = ['Arduino', 'serial', 'usb', 'protocol', 'communication'],
+    classifiers      = [],
+    install_requires = ['pyserial']
 )
